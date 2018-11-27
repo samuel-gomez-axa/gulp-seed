c2a4467c52e5efeca059dbde22d0a95692616559
Samuel Gomez <contact@samuelgomez.fr>
2018-11-28 00:42:27 +0100

    design & fetch

--- gulpfile.babel.js/pug.js
+++ gulpfile.babel.js/pug.js
@@ -3,17 +3,26 @@ import pug from 'gulp-pug';
 import fs from 'fs';
 import { reload } from './serve';
 import config from './config';
+import fetch from 'node-fetch';
+global.fetch = fetch;
+
 const { pathSrc, pathDest, pugFiles } = config;
 
 const basedir = './';
 
-const pugTsk = () => {
+const getRepos = () =>
+  fetch('https://api.github.com/users/axaguildev/repos')
+    .then(res => res.json())
+    .catch(() => console.log('failed'));
+
+const pugTsk = async () => {
   const base = JSON.parse(fs.readFileSync('./src/data/base.json'));
   const general = JSON.parse(fs.readFileSync('./src/data/general.json'));
   const menu = JSON.parse(fs.readFileSync('./src/data/menu.json'));
   const home = JSON.parse(fs.readFileSync('./src/data/home.json'));
+  const repos = await getRepos();
 
-  const data = { base, general, menu, home, basedir };
+  const data = { repos: repos, base, general, menu, home, basedir };
 
   return src(`${pathSrc}${pugFiles}`)
     .pipe(
