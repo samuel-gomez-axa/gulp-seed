5781b829d7b91561e2b8b27f693eb902b229e351
Samuel Gomez <samuel.gomez@axa.fr>
2018-12-13 15:41:04 +0100

    CI

--- gulpfile.babel.js/pug.js
+++ gulpfile.babel.js/pug.js
@@ -2,6 +2,7 @@ import { src, dest } from 'gulp';
 import pug from 'gulp-pug';
 import fs from 'fs';
 import fetch from 'node-fetch';
+import co from 'co';
 import { reload } from './serve';
 import config from './config';
 
@@ -11,18 +12,19 @@ const { pathSrc, pathDest, pugFiles } = config;
 
 const basedir = './';
 const baseData = './src/data/';
+const secretApp = '?client_id=1aa2d218b6767c22cd2a&client_secret=9762db2dfc39e0b0e8d9988b89ebe8ad0221488e';
 
-const fetchRepo = () => fetch('https://api.github.com/users/axaguildev/repos')
+const fetchRepo = () => fetch(`https://api.github.com/users/axaguildev/repos${secretApp}`)
   .then(res => res.json())
   .catch(() => console.log('failed'));
 
-const fetchRepoApi = repo => type => fetch(`https://api.github.com/users/axaguildev/${repo}/${type}`)
+const fetchRepoApi = repo => type => fetch(`https://api.github.com/repos/AxaGuilDEv/${repo}/${type}${secretApp}`)
   .then(res => res.json())
   .catch(() => console.log('failed'));
 
 const getRepos = () => fetchRepo();
-const getContributors = () => fetchRepoApi('react-toolkit')('contributors');
-const getTags = () => fetchRepoApi('react-toolkit')('tags');
+const getContributors = name => fetchRepoApi(name)('contributors');
+const getTags = name => fetchRepoApi(name)('tags');
 
 const getFileData = fileName => JSON.parse(fs.readFileSync(`${baseData}${fileName}.json`));
 
@@ -31,13 +33,26 @@ const pugTsk = async () => {
   const general = getFileData('general');
   const menu = getFileData('menu');
   const home = getFileData('home');
+
   const repos = await getRepos();
-  const contributors = await getContributors();
-  const tags = await getTags();
+
+  const contributorsByRepo = await Promise.all(
+    repos.map(async ({ name }) => ({
+      name,
+      users: await getContributors(name),
+    })),
+  );
+
+  const tagsByRepo = await Promise.all(
+    repos.map(async ({ name }) => ({
+      name,
+      users: await getTags(name),
+    })),
+  );
 
   const data = {
-    tags,
-    contributors,
+    contributorsByRepo,
+    tagsByRepo,
     repos,
     base,
     general,
